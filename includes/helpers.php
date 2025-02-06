b4a49352aec550c0d15518f2a2fa68ffd513390e
Daniel Amado <daniel.amadove@gmail.com>
2025-02-06 08:39:48 -0500

    Improve logging functionality to write messages to a specific log file with timestamps

--- includes/helpers.php
+++ includes/helpers.php
@@ -8,7 +8,13 @@ function wcuph_get_accumulated_hours($user_id) {
 }
 
 function wcuph_log($message) {
-    error_log('[WCUPH] ' . $message);
+    $log_file = WP_CONTENT_DIR . '/wcuph-logs.log'; // Ruta del archivo de log
+
+    // Obtener la fecha en la zona horaria configurada en WordPress
+    $formatted_message = '[' . wp_date('Y-m-d H:i:s', time(), new DateTimeZone('America/Bogota')) . '] ' . $message . PHP_EOL;
+
+    // Escribir en el archivo de log
+    file_put_contents($log_file, $formatted_message, FILE_APPEND | LOCK_EX);
 }
 
 function wcuph_get_cart_total_horas($user_id, $producto_horas_id) {
