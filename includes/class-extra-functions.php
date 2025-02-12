51b30dc26d4fe5c871b530fd440c4246c96bbf2d
Daniel Amado <daniel.amadove@gmail.com>
2025-02-12 12:22:34 -0500

    Add button to schedule assembly on order completion page

--- includes/class-extra-functions.php
+++ includes/class-extra-functions.php
@@ -7,9 +7,11 @@ class ExtraFunctions
   public function __construct()
   {
     add_action('woocommerce_thankyou', [$this, 'auto_complete_paid_orders']);
+    add_action('woocommerce_thankyou', [$this, 'agregar_boton_agendar_ensamble'], 5);
+
   }
 
-  function auto_complete_paid_orders($order_id)
+  public function auto_complete_paid_orders($order_id)
   {
     if (!$order_id) return;
 
@@ -23,4 +25,17 @@ class ExtraFunctions
       $order->update_status('completed');
     }
   }
+
+
+  public function agregar_boton_agendar_ensamble($order_id) {
+    if (!$order_id) return;
+
+    $order = wc_get_order($order_id);
+    $agendar_url = '/ensambles#agenda'; // Cambia esta URL por la correcta
+
+    echo '<div style="margin: 20px 0; text-align: center;">';
+    echo '<a href="' . esc_url($agendar_url) . '" class="btn-agenda-thank">2. Agendar Ensamble</a>';
+    echo '</div>';
+}
+
 }
